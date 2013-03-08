1f59c0e20306380fb96fa0365f74e35280fa9c68
Michael Bestas <mikeioannina@gmail.com>
2013-03-08 05:08:32 +0200

    iyokan: some cleanup
    * Added & fixed some comments to the source files
    * Reorganized alphabetically proprietary files list
    * recovery.fstab: convert tabs to spaces
    * Move gsm feature xml to mogami-common
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Icc6e4f0817c1df31cadf1d025309d93f5de58991

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,13 +1,15 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/iyokan/BoardConfigVendor.mk
 
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_iyokan_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+# Recovery
 TARGET_RECOVERY_INITRC := device/semc/iyokan/recovery/init.rc
 
-#Sensors
+# Sensors
 SOMC_CFG_SENSORS_COMPASS_AK8975 := yes
 SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
 SOMC_CFG_SENSORS_PROXIMITY_APDS9700 := yes
@@ -16,6 +18,6 @@ TARGET_QCOM_HDMI_OUT := false
 
 TARGET_OTA_ASSERT_DEVICE := MK16i,MK16a,iyokan
 
-#Bluetooth
+# Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/iyokan/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/semc/iyokan/bluetooth/vnd_iyokan.txt
