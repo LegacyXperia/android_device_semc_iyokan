1753ee46deb7e984468afe2b2d252ee9f5ebd4c2
Michael Bestas <mikeioannina@gmail.com>
2013-06-07 00:59:42 +0300

    iyokan: remove obsolete XperiaParts
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I5aebf53b4379e59fc533ceae4738066aed41a8bc

--- full_iyokan.mk
+++ full_iyokan.mk
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# XperiaParts for hardware keyboard
-PRODUCT_PACKAGES += \
-    XperiaParts
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
