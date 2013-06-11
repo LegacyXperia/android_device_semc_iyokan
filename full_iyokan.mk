4d73c8bdaf73cb87c643bb25a783e2a83772a221
Michael Bestas <mikeioannina@gmail.com>
2013-06-11 13:47:04 +0300

    Fix build
    Change-Id: I8a8cb4be88eb81cee82acba2cf42b599cbbe5328
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- full_iyokan.mk
+++ full_iyokan.mk
@@ -18,6 +18,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from iyokan device
 $(call inherit-product, device/semc/iyokan/iyokan.mk)
 
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_iyokan
 PRODUCT_DEVICE := iyokan
