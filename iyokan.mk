516b6c7e207ee4c2ffa2d967ae7720fc790901c1
Michael Bestas <mikeioannina@gmail.com>
2013-06-20 19:42:21 +0300

    Proprietary side needs to exist

--- iyokan.mk
+++ iyokan.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # proprietary side of the device
-$(call inherit-product-if-exists, vendor/semc/iyokan/iyokan-vendor.mk)
+$(call inherit-product, vendor/semc/iyokan/iyokan-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/semc/iyokan/overlay
 
