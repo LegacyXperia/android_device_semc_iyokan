bfcf270372efc9da9a2e406b24818839b5822052
Michael Bestas <mikeioannina@gmail.com>
2013-08-20 06:02:49 +0300

    iyokan: switch to mini_phone
    Save 13,5MB in /system by excluding sample videos & some ringtones
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- cm.mk
+++ cm.mk
@@ -19,7 +19,18 @@ $(call inherit-product, device/semc/iyokan/full_iyokan.mk)
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+
+# Optional CM packages
+PRODUCT_PACKAGES += \
+    HoloSpiralWallpaper \
+    MagicSmokeWallpapers \
+    NoiseField \
+    Galaxy4 \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    PhaseBeam
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=MK16i BUILD_FINGERPRINT="SEMC/MK16i_1254-2184/MK16i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" PRIVATE_BUILD_DESC="MK16i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
